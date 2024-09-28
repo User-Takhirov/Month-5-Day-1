b11dce2eebba38b1962f48cf572a68a58132711b
User-Takhirov <tohirovfayruz@gmail.com>
2024-09-29 02:26:19 +0500

    updated

--- src/Components/CommentCard/CommentCard.jsx
+++ src/Components/CommentCard/CommentCard.jsx
@@ -6,12 +6,10 @@ export const CommentCard = ({ title, text }) => {
   return (
     <div>
       <div className={style.block}>
-        <div></div>
         <img className={style.img} src={img} alt="#" />
         <div className={style.content}>
-
-        <h3 className={style.title}>{title}</h3>
-        <p className={style.text}>{text}</p>
+          <h3 className={style.title}>{title}</h3>
+          <p className={style.text}>{text}</p>
         </div>
       </div>
     </div>
